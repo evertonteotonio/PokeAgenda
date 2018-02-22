e1e3531fccac1dcb2de32c531fe055c7e4bc2441
logonrm <logonrm@fiap.com.br>
2018-02-21 22:30:36 -0300

    inicializando os layouts

--- app/src/main/java/com/example/logonrm/pokeagenda/MainActivity.kt
+++ app/src/main/java/com/example/logonrm/pokeagenda/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.logonrm.pokeagenda
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.logonrm.pokeagenda.api.PokemonAPI
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Retrofit
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btPesquisar.setOnClickListener{
+            pesquisarPokemon()
+        }
+
     }
+
+    fun pesquisarPokemon() {
+        val retrofit = Retrofit.Builder()
+                .baseUrl("https://pokeapi.co")
+                .build()
+
+        val  api = retrofit.create(PokemonAPI::class.java)
+    }
+
 }
